d98a8f172bc02796018080d37ec53a006cbad3ae
mcgidoug <mcgidoug@gmail.com>
2023-02-26 14:06:15 -0600

    initial commit

---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const shop = () => {
+  return (
+    <div className="shop">
+      <div className="shopTitle">
+        <h1>Adriel Lopez Storefront</h1>
+      </div>
+      <div className="products"></div>
+    </div>
+  );
+};
+
+export default shop;
---
+++ src/products.js
@@ -0,0 +1,10 @@
+import product1 from "./assets/desk.png";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "iPhone",
+    price: 999.0,
+    productImage: product1,
+  },
+];

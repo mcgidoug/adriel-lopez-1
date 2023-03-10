1cb4f5c15de7cde8955b59dc671ae7d25ca08a8c
mcgidoug <mcgidoug@gmail.com>
2023-03-10 11:42:55 -0600

    completed main functionality of site

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -3,9 +3,12 @@ import PRODUCTS from "../../products";
 import ShopContext from "../../context/shop-context";
 import CartItem from "./cart-item";
 import "./cart.css";
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
-  const { cartItems } = useContext(ShopContext);
+  const { cartItems, getTotalCartAmount } = useContext(ShopContext);
+  const totalAmount = getTotalCartAmount();
+  const navigate = useNavigate();
   return (
     <div className="cart">
       <div>
@@ -15,14 +18,20 @@ const Cart = () => {
         {PRODUCTS.map((product) => {
           if (cartItems[product.id] !== 0) {
             return <CartItem data={product} />;
+          } else {
+            return null;
           }
         })}
       </div>
-      <div className="checkout">
-        <p>Subtotal: $</p>
-        <button>Continue Shopping</button>
-        <button>Checkout</button>
-      </div>
+      {totalAmount > 0 ? (
+        <div className="checkout">
+          <p>Subtotal: ${totalAmount}</p>
+          <button onClick={() => navigate("/")}>Continue Shopping</button>
+          <button>Checkout</button>
+        </div>
+      ) : (
+        <h3>Your Cart Is Empty</h3>
+      )}
     </div>
   );
 };
